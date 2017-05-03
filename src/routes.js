a16006b2350341b850b4ac69dd0e125d45efa753
petahcole <cbatson89@gmail.com>
2017-05-03 14:00:25 -0600

    creating form child components

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import Layout from './components/Layout.js';
 import HomePage from './components/home/HomePage.js';
 import AboutPage from './components/about/AboutPage.js';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage'
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomePage} />
-    <Route path="about" component={AboutPage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage} />
+    <Route path="course/:id" component={ManageCoursePage} />
+    <Route path="about" component={AboutPage} />
   </Route>
 );
