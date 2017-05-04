5d1be9e3c9541ba64ee3bc0fb53ebf179fdfa32e
petahcole <cbatson89@gmail.com>
2017-05-04 13:13:53 -0600

    manage courses

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,14 @@ export function loadCoursesSuccess(courses)  {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function updateCourseSuccess(course)  {
+  return { type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function createCourseSuccess(course)  {
+  return { type: types.CREATE_COURSE_SUCCESS, course};
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses =>  {
@@ -13,4 +21,16 @@ export function loadCourses() {
       throw(error);
     });
   }
+}
+
+export function saveCourse(course)  {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse  =>  {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+          dispatch(createCourseSuccess(savedCourse))
+    })
+    .catch(error  =>  {
+      throw(error)
+    });
+  };
 }
